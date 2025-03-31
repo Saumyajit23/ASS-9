a2da914c55179c9b1945c139bfeb2dfa8dd0ea9f
Saumyajit23 <ghoshsaumyajit2302@gmail.com>
2025-03-31 23:13:49 +0530

    index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello MCKV bam bhola')
+  res.send('Hello MCKV 69')
 })
 
 app.get('/courses', function (req, res) {
